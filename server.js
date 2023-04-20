692c4c58eb08dc92b19256c584312b25d358febe
Namaniel <nathaniel.nguyen@unc.edu>
2023-04-20 01:40:54 -0400

    when will i be able to go to sleep

--- server.js
+++ server.js
@@ -43,16 +43,17 @@ fs.readFile('./public/index.html', 'utf8', (err, data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
-const server = http.createServer(function(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(temp);
-});
+})
+
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
-server.listen(port);
-console.log(`Server listening on port ${port}`);
+server.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
 
 // That's it! You're all done!
